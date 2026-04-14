6cd2b98bb05ff14afce4e4e73c32ef8a6600f8e8
adrielly <tavernisdemoura@gmail.com>
2026-04-14 08:44:19 -0300

    Create cabeçalho.js

---
+++ projeto/cabeçalho.js
@@ -0,0 +1,7 @@
+console.log("Cabeçalho do projeto");
+console.log("Escola do futuro José Luiz bittencourt");
+console.log("Curso técnico em Desenvolvimento Web e cybersegurança");
+console.log("Disciplina: Back-end");
+console.log("Professor: Geoger Mendes Marra");
+console.log("Projeto: Criando um Servidor com web básico Node.js");
+console.log("Integrantes: Adrielly,Ana freitas,Ana beatriz,Atila,Beatriz de sousa ");
